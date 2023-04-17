b49e41f7ac04177ae7e303ad227e793bba638fdf
Michael Xu <michaelx@xmtp.com>
2023-04-17 10:48:27 -0700

    test: also use localhost:5556 for test

--- Tests/XMTPRust-Tests/XMTPRust_Tests.swift
+++ Tests/XMTPRust-Tests/XMTPRust_Tests.swift
@@ -6,7 +6,7 @@ import XCTest
 // See if we can use the XMTPRust library
 class XMTPRust_Tests: XCTestCase {
     func testExample() async throws {
-        let apiservice = XMTPRust.ApiService(environment: "http://localhost:15555", secure: false)
+        let apiservice = XMTPRust.ApiService(environment: "http://localhost:5556", secure: false)
         let queryResult = try await apiservice.query(topic: "test", json_paging_info: "")
         XCTAssertFalse(queryResult.isEmpty)
     }
